b24cf60577112de83894f955f627f3cacac7cf13
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-11-21 13:26:58 +0600

    project initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   content: [
@@ -14,5 +15,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
