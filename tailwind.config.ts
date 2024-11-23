b88c5054fe5c1afe6bfb5108064b912f5c78fb1d
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-11-23 23:33:20 +0600

    initial form added from shad cn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,25 +2,31 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
+  	extend: {
+  		colors: {
+  			brand: {
   				'100': '#424874',
   				DEFAULT: '#F4EEFF'
   			},
-        purple: '#A6B1E1',
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  			purple: '#A6B1E1',
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [tailwindcssAnimate, require("tailwindcss-animate")],
 } satisfies Config;
 
 
